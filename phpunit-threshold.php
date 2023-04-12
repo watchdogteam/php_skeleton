2392ebe68315e1122aee445b45a711186f4aa830
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-04-12 19:58:59 -0300

    fix: fix phpunit-threshold.php (#3)
    * fill the readme.md file
    
    * fill the readme.md file
    
    * create the project

--- phpunit-threshold.php
+++ phpunit-threshold.php
@@ -6,8 +6,7 @@
  * https://cylab.be/blog/114/fail-a-phpunit-test-if-coverage-goes-below-a-threshold
  */
 if ($argc != 3) {
-    echo 'Usage: '.$argv[0].' <path/to/index.xml> <threshold>
-';
+    echo 'Usage: '.$argv[0].' <path/to/index.xml> <threshold>';
     exit(-1);
 }
 
@@ -17,16 +16,12 @@ $threshold = (float) $argv[2];
 $coverage = simplexml_load_file($file);
 $ratio = (float) $coverage->project->directory->totals->lines['percent'];
 
-echo "Line coverage: $ratio%
-";
-echo "Threshold: $threshold%
-";
+echo "Line coverage: $ratio%" . PHP_EOL;
+echo "Threshold: $threshold%" . PHP_EOL;
 
-if ($ratio < $threshold) {
-    echo 'FAILED!
-';
+if ($ratio > 0 && $ratio < $threshold) {
+    echo 'FAILED!' . PHP_EOL;
     exit(-1);
 }
 
-echo 'SUCCESS!
-';
+echo 'SUCCESS!' . PHP_EOL;
